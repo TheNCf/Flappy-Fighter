2e4bb0631a617cadb37b4d74b99c12538bae5ff6
TheNCf <nikitadorofeevandreevich2000@gmail.com>
2025-08-08 00:13:25 +0300

    Added mover script for player

--- Assets/Game/Scripts/Characters/Player/PlayerMover.cs
+++ Assets/Game/Scripts/Characters/Player/PlayerMover.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _altitudeGainAmount = 5.0f;
+    [Space(10)]
+    [SerializeField] private float _maxTilt = 20.0f;
+    [SerializeField] private float _minTilt = -25.0f;
+    [SerializeField] private float _clickTiltSpeed = 10.0f;
+    [SerializeField] private float _minTiltSpeed = 0.5f;
+    [SerializeField] private float _decreaseTiltSpeed = 0.5f;
+
+    private Rigidbody2D _rigidbody;
+
+    private float _desiredTilt = 0.0f;
+    private float _desiredTiltSpeed;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _desiredTiltSpeed = _decreaseTiltSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        ChangeAngle();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GainAltitude();
+        }
+    }
+
+    public void ChangeAngle()
+    {
+        _desiredTiltSpeed = Mathf.Clamp(_desiredTiltSpeed - _decreaseTiltSpeed * Time.deltaTime, _minTiltSpeed, _clickTiltSpeed);
+        _desiredTilt = Mathf.Lerp(_desiredTilt, _minTilt, _desiredTiltSpeed * Time.deltaTime);
+        transform.localEulerAngles = new Vector3(0.0f, 0.0f, _desiredTilt);
+    }
+
+    public void GainAltitude()
+    {
+        _rigidbody.velocity = new Vector2(0.0f, _altitudeGainAmount);
+        _decreaseTiltSpeed = _clickTiltSpeed;
+        _desiredTilt = _maxTilt;
     }
 }
